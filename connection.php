9db851b43d8fbe9dd90a95bb41845a99f0c6cbde
RobC4D <robin.maquaire@gmail.com>
2017-08-11 17:20:13 +0100

    change1

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$vHost="localhost:3307";
-// NOM DE LA BASE DE DONNEES A MODIFIER AVEC LE VOTRE
-$vDbname="testkit";
-$vUser="root";
-$vPassword="root";
-$vConn = mysqli_connect($vHost,$vUser,$vPassword,$vDbname);
-mysqli_query($vConn, "SET NAMES UTF8");
-?>
